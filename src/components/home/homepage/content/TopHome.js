c3d9f53be9fc85a63e84619238ea57062cb44584
Annie Alfred <fredileader@gmail.com>
2022-05-05 17:20:04 +0100

    arranged the sampled NFTs properly with card and carousel

--- src/components/home/homepage/content/TopHome.js
+++ src/components/home/homepage/content/TopHome.js
@@ -1,59 +1,170 @@
-import React from 'react'
-import './TopHome.css'
-import image1 from './image1.jpg'
-import load from './image2.jpg'
-import world from './image3.jpg'
+import React from "react";
+import "./TopHome.css";
+import image1 from "@asset/images/image1.jpg";
+import load from "@asset/images/image2.jpg";
+import world from "@asset/images/image3.jpg";
 
 const TopHome = () => {
   return (
-    <div id="carouselExampleControls" className="carousel slide" 
-        data-bs-ride="carousel">
-
+    <div
+      id="carouselExampleControls"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
       <div className="carousel-inner">
-
         <div className="carousel-item active">
-
-          <img src={ image1 } className="d-block h-30 w-100 image" alt="..."/>
-
+          <div className="cards-wrapper">
+            <div className="card">
+              <img src={image1} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img src={world} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img src={load} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="carousel-item">
-
-          <img src={ load } className="d-block h-30 w-100 image" alt="..."/>
-
+          <div className="cards-wrapper">
+            <div className="card">
+              <img src={load} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img src={image1} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img src={world} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="carousel-item">
-
-          <img src={ world } className="d-block h-30 w-100 image" alt="..."/>
-
+          <div className="cards-wrapper">
+            <div className="card">
+              <img src={world} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img src={load} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img src={image1} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
-
-      <button className="carousel-control-prev" 
-              type="button" data-bs-target="#carouselExampleControls" 
-              data-bs-slide="prev">
-
-        <span className="carousel-control-prev-icon" 
-              aria-hidden="true"></span>
-
+      
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
-
       </button>
-
-      <button className="carousel-control-next" 
-              type="button" data-bs-target="#carouselExampleControls" 
-              data-bs-slide="next">
-
-        <span className="carousel-control-next-icon" 
-              aria-hidden="true"></span>
-
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
-
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default TopHome
+export default TopHome;
