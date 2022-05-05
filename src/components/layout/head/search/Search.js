17333b3daaff534e184cf6551330d41d27efe5fa
Annie Alfred <fredileader@gmail.com>
2022-05-05 17:20:30 +0100

    second commit

--- src/components/layout/head/search/Search.js
+++ src/components/layout/head/search/Search.js
@@ -3,12 +3,7 @@ import './Search.css';
 const Search = () => {
   return (
     <div className="input-group">
-
-      <span className="input-group-text" id="basic-addon1">
-        <i className="fas fa-search"></i>
-      </span>
-
-      <input type="text" className="form-control" 
+      <input type="text" className="form-control w-50 p-3" 
       placeholder="Search" aria-label="Search" 
       aria-describedby="basic-addon1" />
     </div>
