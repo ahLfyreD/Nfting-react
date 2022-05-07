a7a931478dd52f992a3be8d5d3b8d2a7be26d704
Annie Alfred <fredileader@gmail.com>
2022-05-07 14:58:48 +0100

    added and styled with card format

--- src/components/home/homepage/content/About.js
+++ src/components/home/homepage/content/About.js
@@ -4,7 +4,51 @@ const Footer = () => {
   return (
     <div className="create">
     <h2>About NFTing</h2>
-    <div className="set">
+
+    <div className="row mx-10 mt-3">
+            {/* Each column contains components with images, a radio button and a name of the collector. */}
+            <div className="col mx-4">
+              <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                <div class="card-body">
+                  <h5 class="card-title" style={{textAlign: 'center'}}>Advantages</h5>
+                  <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo beatae pariatur facilis. Recusandae corrupti optio tempora quos. Debitis optio ducimus eos fugiat ullam, nostrum omnis impedit dolore, dolorem excepturi vitae?</p>
+                  
+                </div>
+              </div>
+            </div>
+            <div className="col mx-4">
+                <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                    <div class="card-body">
+                        <h5 class="card-title" style={{textAlign: 'center'}}>Secure</h5>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo beatae pariatur facilis. Recusandae corrupti optio tempora quos. Debitis optio ducimus eos fugiat ullam, nostrum omnis impedit dolore, dolorem excepturi vitae?</p>
+                  
+                    </div>
+                </div>            
+            </div>
+            <div className="col mx-4">
+                <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                    <div class="card-body">
+                        <h5 class="card-title " style={{textAlign: 'center'}}>Socialise</h5>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo beatae pariatur facilis. Recusandae corrupti optio tempora quos. Debitis optio ducimus eos fugiat ullam, nostrum omnis impedit dolore, dolorem excepturi vitae?</p>
+                  
+                    </div>
+                </div>            
+            </div>
+            <div className="col mx-4">
+                <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                    <div class="card-body">
+                        <h5 class="card-title" style={{textAlign: 'center'}}>Freedom</h5>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo beatae pariatur facilis. Recusandae corrupti optio tempora quos. Debitis optio ducimus eos fugiat ullam, nostrum omnis impedit dolore, dolorem excepturi vitae?</p>
+                  
+                    </div>
+                </div>             
+            </div>
+    </div>
+    {/* <div className="set">
         <div className="content">
             <div className="display-content"></div>
             <h3>Advantages</h3>
@@ -39,10 +83,10 @@ const Footer = () => {
 
         </div>
         
+    </div> */}
+
+
     </div>
-
-
-</div>
   )
 }
 
