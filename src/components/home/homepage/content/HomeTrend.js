30f4bc970837169238b34501354eaa9ac13bfb54
Annie Alfred <fredileader@gmail.com>
2022-05-07 15:04:29 +0100

    added and styled with card format

--- src/components/home/homepage/content/HomeTrend.js
+++ src/components/home/homepage/content/HomeTrend.js
@@ -20,12 +20,56 @@ const HomeTrend = () => {
 
         <div className="row mx-5 mt-5">
             {/* Each column contains components with images, a radio button and a name of the collector. */}
-            <div className="col">col-1</div>
-            <div className="col">col-2</div>
-            <div className="col">col-3</div>
-            <div className="col">col-4</div>
-            <div className="col">col-5</div>
-            <div className="col">col-6</div>
+            <div className="col">
+              <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="/" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+            <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="/" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>            
+            </div>
+            <div className="col">
+            <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="/" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>            
+            </div>
+            <div className="col">
+            <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="/" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>             
+           </div>
+            <div className="col">
+            <div class="card" style={{width: '10rem'}}>
+                {/* <img src="..." class="card-img-top" alt="..."/> */}
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="/" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>             
+            </div>
         </div>
     
        
