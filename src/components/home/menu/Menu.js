1095e5845a57fc34d8df149d05273b921834c4f2
Annie Alfred <fredileader@gmail.com>
2022-05-07 15:20:08 +0100

    added icons and styled with alignment.

--- src/components/home/menu/Menu.js
+++ src/components/home/menu/Menu.js
@@ -5,12 +5,12 @@ const Menu = () => {
   return (
     <div className="menu">
         <ul>
-            <li><Link className="link" to="/">Home Page</Link></li>
-            <li><Link className="link" to="/mynft">My NFT</Link></li>
-            <li><Link className="link" to="/create">Create/Mint NFT</Link></li>
-            <li><Link className="link" to="/newsfeed">NFT News Feed</Link></li>
-            <li><Link className="link" to="/liveevent">Live Event</Link></li>
-            <li><Link className="link" to="/marketstatistics">Market Statistics</Link></li>
+            <li><Link className="link" to="/"><i class="fas fa-home"></i>Home Page</Link></li>
+            <li><Link className="link" to="/mynft"><i class="fas fa-receipt"></i>My NFT</Link></li>
+            <li><Link className="link" to="/create"><i class="far fa-plus-square"></i>Create/Mint NFT</Link></li>
+            <li><Link className="link" to="/newsfeed"><i class="fas fa-newspaper"></i>NFT News Feed</Link></li>
+            <li><Link className="link" to="/liveevent"><i class="fas fa-rss"></i>Live Event</Link></li>
+            <li><Link className="link" to="/marketstatistics"><i class="fas fa-angle-double-up"></i>Market Statistics</Link></li>
         </ul>
 
     </div>
